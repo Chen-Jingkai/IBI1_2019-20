452661b8c8574c54426237085d0f81ab16142abc
Chen Jingkai <jingkai.19@intl.zju.edu.cn>
2020-05-15 11:59:36 +0800

    practical check

--- Practical 10/Practical 10.py
+++ Practical 10/Practical 10.py
@@ -28,7 +28,6 @@ mouse_file.close()
 random_file.close()
 matrix_file.close()
 
-
 import re
 matrix=matrix.split('\n')
 order_list=matrix[0]
@@ -36,21 +35,21 @@ order_list=order_list.lstrip()
 order_list=re.split(r'\s+',order_list)
 
 
-def order(b):
+def ord(b):
     for t in range(1,24):
         if order_list[t-1]==b:
             return(t)
             break
 
-def compare(seq1,seq2,name1,name2):
+def com(seq1,seq2,name1,name2):
     
     alignment=''
     score=0
     count=0
     
     for i in range(len(human)-1):
-        order1=order(seq1[i])
-        order2=order(seq2[i])
+        order1=ord(seq1[i])
+        order2=ord(seq2[i])
         line=matrix[order1]
         line=re.split(r'\s+',line)
         value=int(line[order2])
@@ -71,7 +70,7 @@ def compare(seq1,seq2,name1,name2):
 
 
 output.write('comparison between human and mouse\n\n')
-compare(human,mouse,'human ','mouse ')
+com(human,mouse,'human ','mouse ')
 output.write('comparison between human and random\n\n')
 compare(human,random,'human ','random')
 output.write('comparison between mouse and random\n\n')
